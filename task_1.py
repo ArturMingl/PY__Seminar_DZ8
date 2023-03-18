61e96ad74f7e66db2e5d22b16ed29d15364bf5e6
ArturMingl <adiscov90@gmail.com>
2023-03-18 18:12:07 +0300

    task1 done

--- task_1.py
+++ task_1.py
@@ -23,15 +23,12 @@ def type_and_content_print(lst):
         print(f'{i} - {type(i)}')
 
 
-def conv_to_points(lst):
-    pointlst = []
-    for word in lst:
-        bw = word.encode('unicode_escape')
-        cw = bw.decode()
-        pointlst.append(cw)
-    return pointlst
-
-
 wlst = ['разработка', 'сокет', 'декоратор']
+ulist = ['\u0440\u0430\u0437\u0440\u0430\u0431\u043e\u0442\u043a\u0430',
+         '\u0441\u043e\u043a\u0435\u0442',
+         '\u0434\u0435\u043a\u043e\u0440\u0430\u0442\u043e\u0440']
 type_and_content_print(wlst)
-type_and_content_print(conv_to_points(wlst))
+type_and_content_print(ulist)
+
+
+
